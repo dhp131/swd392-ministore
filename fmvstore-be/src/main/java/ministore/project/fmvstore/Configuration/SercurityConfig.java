162d329e76ea8acf8f2bd8f66355d3926c0b5210
nguyenletam026 <nguyenletam026@gmail.com>
2024-10-07 22:24:31 +0700

    PASSWORD_ENCODE

---
+++ fmvstore-be/src/main/java/ministore/project/fmvstore/Configuration/SercurityConfig.java
@@ -0,0 +1,14 @@
+package ministore.project.fmvstore.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SercurityConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
--- fmvstore-be/src/main/java/ministore/project/fmvstore/User/UserService.java
+++ fmvstore-be/src/main/java/ministore/project/fmvstore/User/UserService.java
@@ -1,15 +1,18 @@
 package ministore.project.fmvstore.User;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 
+
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     public void createUser(UserCreationRequest request) {
         UserEntity userEntity = new UserEntity();
@@ -17,7 +20,7 @@ public class UserService {
         userEntity.setFirstName(request.getFirstName());
         userEntity.setLastName(request.getLastName());
         userEntity.setEmail(request.getEmail());
-        userEntity.setPassword(request.getPassword());
+        userEntity.setPassword(passwordEncoder.encode(request.getPassword()));
         userEntity.setNumber(request.getNumber());
         userRepository.save(userEntity);
     }
