d635da7b905a40a0e29c8ff5c39e085f3f2c8520
nguyenletam026 <nguyenletam026@gmail.com>
2024-10-24 00:14:18 +0700

    cloudinary

--- fmvstore-be/src/main/java/ministore/project/fmvstore/Product/ProductController.java
+++ fmvstore-be/src/main/java/ministore/project/fmvstore/Product/ProductController.java
@@ -3,11 +3,14 @@ package ministore.project.fmvstore.Product;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import ministore.project.fmvstore.Cloudinary.CloudinaryService;
 import ministore.project.fmvstore.Response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,8 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ProductController {
-
-
+    final CloudinaryService cloudinaryService;
     private ProductService productService;
     @GetMapping
     public List<Product> getAllProducts() {
@@ -25,8 +27,12 @@ public class ProductController {
 
     @PreAuthorize("hasRole('MANAGER') or hasRole('EMPLOYEE')" )
     @PostMapping
-    public ResponseEntity<ApiResponse<String>> createProduct(@RequestBody ProductCreationRequest productCreationRequest) {
-        productService.createProduct(productCreationRequest);
+    public ResponseEntity<ApiResponse<String>> createProduct(
+            @RequestPart("product_detail") ProductCreationRequest request,
+            @RequestPart("product_image") MultipartFile file) throws IOException {
+        String imageUrl = cloudinaryService.uploadImage(file);
+        request.setImageUrl(imageUrl);
+        productService.createProduct(request);
         return ResponseEntity.ok(ApiResponse.<String>builder()
                 .result("Product Create Successfully")
                 .build());
